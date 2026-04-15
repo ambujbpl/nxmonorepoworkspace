71d1999e904f595a16584e1f3b1bd43fe23acee3
ambujdubeybpl <ambujdubey89@gmail.com>
2026-04-15 10:04:06 +0530

    comment webkit configuration

--- apps/frontend-e2e/playwright.config.ts
+++ apps/frontend-e2e/playwright.config.ts
@@ -51,11 +51,12 @@ export default defineConfig({
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    
+    // Uncomment for Safari support on macOS
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     // Uncomment for mobile browsers support
     /* {
