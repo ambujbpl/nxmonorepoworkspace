034375c39e640f418eed063080f8c85950b770ed
ambujdubeybpl <ambujdubey89@gmail.com>
2026-04-18 20:31:55 +0530

    fix pipeline issue

--- apps/backend/src/app/auth.util.ts
+++ apps/backend/src/app/auth.util.ts
@@ -1,10 +1,10 @@
 import {
   createHmac,
   randomBytes,
-  scrypt,
+  scryptSync,
   timingSafeEqual,
 } from 'crypto';
-import { promisify } from 'util';
+
 import { UnauthorizedException } from '@nestjs/common';
 
 export type JwtPayload = {
@@ -15,23 +15,22 @@ export type JwtPayload = {
 };
 
 const DEFAULT_JWT_EXPIRES_IN_SECONDS = 60 * 60 * 24;
-const scryptAsync = promisify(scrypt);
 
-export async function hashPassword(password: string) {
+export function hashPassword(password: string) {
   const salt = randomBytes(16).toString('hex');
-  const hashedPassword = (await scryptAsync(password, salt, 64)).toString('hex');
+  const hashedPassword = scryptSync(password, salt, 64).toString('hex');
 
   return `${salt}:${hashedPassword}`;
 }
 
-export async function verifyPassword(password: string, storedPassword: string) {
+export function verifyPassword(password: string, storedPassword: string) {
   const [salt, storedHash] = storedPassword.split(':');
 
   if (!salt || !storedHash) {
     return false;
   }
 
-  const derivedKey = (await scryptAsync(password, salt, 64)) as Buffer;
+  const derivedKey = scryptSync(password, salt, 64);
   const storedKey = Buffer.from(storedHash, 'hex');
 
   if (derivedKey.length !== storedKey.length) {
